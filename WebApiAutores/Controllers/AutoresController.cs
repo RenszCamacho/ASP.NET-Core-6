4883c13668e2eb96ec2b8fe2673b906ed7ee14ad
Renszo D'Giovanni <varsovia84@gmail.com>
2022-08-06 17:52:18 +0200

    Create a basic controller

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Name = "John"},
+
+                new Autor() { Id = 2, Name = "Sean"}
+            };
+        }
+    }
+}
