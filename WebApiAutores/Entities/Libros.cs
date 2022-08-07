0a73b1fd418f73dcef99adfa14633aa15d1b6c24
Renszo D'Giovanni <varsovia84@gmail.com>
2022-08-07 08:50:59 +0200

    Craete new Entity, Libros

---
+++ WebApiAutores/Entities/Libros.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAutores.Entities
+{
+    public class Libros
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Autor Author { get; set; }
+    }
+}
