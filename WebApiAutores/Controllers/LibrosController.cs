6d69ced6a2c9a8bccea82cc10d144d6ed6a2cbb0
Renszo D'Giovanni <varsovia84@gmail.com>
2022-08-11 21:25:19 +0200

    Create LibrosController

---
+++ WebApiAutores/Controllers/LibrosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetItem")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await _context.Libros
+                .Include(x => x.Author)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            var isThereAuthor = await _context.Autores.AnyAsync(x => x.Id == libro.AuthorId);
+
+            if (!isThereAuthor)
+            {
+                return BadRequest($"No existe el autor de Id: {libro.AuthorId}");
+            }
+
+            _context.Add(libro);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
