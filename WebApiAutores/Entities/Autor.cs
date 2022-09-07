9fa84563c6362371e77282e0aafd97d5ef66aa33
Renszo D'Giovanni <varsovia84@gmail.com>
2022-09-07 05:17:48 +0200

    Validaciones por defecto

--- WebApiAutores/Entities/Autor.cs
+++ WebApiAutores/Entities/Autor.cs
@@ -1,9 +1,23 @@
-﻿namespace WebApiAutores.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entities
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener mas de {1} caracteres.")]
         public string Name { get; set; }
+        [Range(18,120)]
+        [NotMapped]
+        public int Age { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string CreditCard { get; set; }
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
         public List<Libro> Libros { get; set; }
 
     }
