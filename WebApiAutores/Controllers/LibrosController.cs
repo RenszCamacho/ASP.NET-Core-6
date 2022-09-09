14e1c72f0dbeebf0b0e054e7b397634c236b603d
Renszo D'Giovanni <varsovia84@gmail.com>
2022-09-09 07:47:02 +0200

    Creando la entidad Libro.

--- WebApiAutores/Controllers/LibrosController.cs
+++ WebApiAutores/Controllers/LibrosController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.DTOs;
+using WebApiAutores.Entities;
 
 namespace WebApiAutores.Controllers
 {
@@ -7,33 +11,37 @@ namespace WebApiAutores.Controllers
     public class LibrosController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public LibrosController(ApplicationDbContext context)
+        public LibrosController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        /* [HttpGet("GetItem")] */
-        /* public async Task<ActionResult<Libro>> Get(int id) */
-        /* { */
-        /*     return await _context.Libros */
-        /*         .Include(x => x.Author) */
-        /*         .FirstOrDefaultAsync(x => x.Id == id); */
-        /* } */
+        [HttpGet("GetItem")]
+        public async Task<ActionResult<LibroDTO>> Get(int id)
+        {
+            var libro = await _context.Libros.FirstOrDefaultAsync(libroDB => libroDB.Id == id);
 
-        /* [HttpPost("Create")] */
-        /* public async Task<ActionResult> Post(Libro libro) */
-        /* { */
-        /*     var isThereAuthor = await _context.Autores.AnyAsync(x => x.Id == libro.AuthorId); */
+            return _mapper.Map<LibroDTO>(libro);
+        }
 
-        /*     if (!isThereAuthor) */
-        /*     { */
-        /*         return BadRequest($"No existe el autor de Id: {libro.AuthorId}"); */
-        /*     } */
+        [HttpPost("Create")]
+        public async Task<ActionResult> Post(LibroCreacionDTO libroCreacionDTO)
+        {
+            /* var isThereAuthor = await _context.Autores.AnyAsync(x => x.Id == libro.AuthorId); */
 
-        /*     _context.Add(libro); */
-        /*     await _context.SaveChangesAsync(); */
-        /*     return Ok(); */
-        /* } */
+            /* if (!isThereAuthor) */
+            /* { */
+            /*     return BadRequest($"No existe el autor de Id: {libro.AuthorId}"); */
+            /* } */
+
+            var libro = _mapper.Map<Libro>(libroCreacionDTO);
+
+            _context.Add(libro);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
